2f84d025be61adcfdd7140ce54fee124e82a57e5
arminder789 <50153291+arminder789@users.noreply.github.com>
2019-06-22 10:06:18 -0400

    Update HelloWorldNew.java

--- Sober/src/day1/HelloWorldNew.java
+++ Sober/src/day1/HelloWorldNew.java
@@ -5,6 +5,8 @@ public class HelloWorldNew {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World New");
+		
+		System.out.println("Hello World New And Practice");
 
 	}
 
